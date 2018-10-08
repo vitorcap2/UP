4754bc053b963f7d3ccef3cb4f5f625014b9fcee
Vitor <vitor-s2000@hotmail.com>
2018-10-08 09:39:35 -0300

    Criando Variaveis

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -1,11 +1,26 @@
 // Aula0810.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+/*
+Comentário longo, bloco	
+(mais de uma linha)
+*/
+
+#include "stdafx.h" // inclusao de biblioteca
+// C ANSI
+#include <stdio.h>
+#include <stdlib.h>
+
+int a; // inteiro
+float b; // real (ponto flutuante)
+double c; // real (maior precisao)
+bool d; // booleano (verdadeiro/ falso) (true/false)
+
 
 
 int main()
 {
+	int e; // variavel local (visivel somente nesa funcao, a partir dessa linha)
     return 0;
 }
 
