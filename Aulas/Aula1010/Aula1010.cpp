747b2cc17ade508c48668fbe79184600ae576560
Vitor <vitor-s2000@hotmail.com>
2018-10-10 10:25:15 -0300

    Termino da aula

--- Aulas/Aula1010/Aula1010.cpp
+++ Aulas/Aula1010/Aula1010.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
 
+void HelloWorld() //void não tem retorno
+{
+	printf("Hello world!\n");
+}
+
+int Soma2Valores(int a, int b)
+{
+	/* int c;
+	c = a + b;
+	return c; */
+
+	return a + b;
+}
 
 int main()
 {
+	HelloWorld();
+	int e, f;
+	printf("Informe os valores\n");
+	scanf_s("%i", &e);
+	scanf_s("%i", &f);
+	int d = Soma2Valores(e, f);
+
+	printf("\nResultado: %i\n", d);
+
+ 	system("pause");
     return 0;
 }
 
